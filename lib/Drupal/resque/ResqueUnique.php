c62176ec4b0699e8c0f9ff57b085c292f14f087d
Arturo Contreras <arturo.kontreras@gmail.com>
2014-08-06 10:46:40 -0500

    Remove xautoload dependency and unique queue module dependency.

--- lib/Drupal/resque/ResqueUnique.php
+++ lib/Drupal/resque/ResqueUnique.php
@@ -6,26 +6,28 @@
 
 namespace Drupal\resque;
 
-use UniqueQueueInterface;
 use Resque_Event;
 use Resque as Php_Resque;
 
-class ResqueUnique extends Resque implements UniqueQueueInterface {
+class ResqueUnique extends Resque {
   /**
    * {@inheritdoc}
    */
   public function createUniqueItem($key, $data) {
     $token = NULL;
 
+    $queues = module_invoke_all('cron_queue_info');
+    drupal_alter('cron_queue_info', $queues);
+
     // Check to see if class name was specified in the data array.
     if (!empty($data['class_name'])) {
       $this->className = $data['class_name'];
     }
-    $queues = module_invoke_all('cron_queue_info');
-    drupal_alter('cron_queue_info', $queues);
+    elseif (!empty($data['worker_callback'])) {
+      // Add the worker callback.
+      $data['worker_callback'] = $queues[$this->name]['worker callback'];
+    }
 
-    // Add the worker callback.
-    $data['worker_callback'] = $queues[$this->name]['worker callback'];
     $data['drupal_unique_key'] = $this->name . ':' . $key;
     Resque_Event::listen(
       'beforeEnqueue',
