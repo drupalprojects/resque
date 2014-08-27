29801ee5262ee514558dafd7dc22065f28186335
Zack Urben <zackurben@gmail.com>
2014-08-27 10:22:43 -0500

    Correcting .info file, updating key acquisition to use redis setnx.

--- lib/Drupal/resque/LockPlugin.php
+++ lib/Drupal/resque/LockPlugin.php
@@ -2,37 +2,46 @@
 /**
  * @file
  * Contains Drupal\resque\PerformLockPlugin.
+ *
+ * Provides a resource lock for the Resque Job Perform function.
  */
 
 namespace Drupal\resque;
 
-use Resque as Php_Resque;
-use Resque_Job;
+use \Resque as Php_Resque;
+use \Resque_Job;
+use \Resque_Job_DontPerform;
 
+/**
+ * Provides semaphore-like behavior for Reque Jobs, by using the event hooks.
+ */
 class LockPlugin {
-
   /**
    * Check if the job can acquire the processing lock.
    *
-   * @param object $job
-   *   The resque job
+   * This will requeue the given job upon failure, if the requeue parameter is
+   * set to true.
+   *
+   * @param Resque_Job $job
+   *   The resque job.
    *
    * @return bool
-   *   True, if the job was acquired, else false and requeue the job.
+   *   If the job was acquired.
+   *
+   * @throws Resque_Job_DontPerform
    */
   public static function beforePerform(Resque_Job $job) {
-    if (Php_Resque::redis()->exists($job->payload['args'][0]['drupal_unique_key'])) {
-      if ($job->payload['args'][0]['requeue']) {
-        Php_Resque::enqueue($job->queue, $job->payload['class'], $job->payload['args'][0], TRUE);
-        return FALSE;
-      }
-      else {
-        return FALSE;
-      }
+    if (Php_Resque::redis()->setnx($job->payload['args'][0]['drupal_unique_key'], '1')) {
+      return TRUE;
     }
     else {
-      Php_Resque::redis()->set($job->payload['args'][0]['drupal_unique_key'], '1');
-      return TRUE;
+      if ($job->payload['args'][0]['requeue']) {
+        Php_Resque::enqueue($job->queue, $job->payload['class'], $job->payload['args'][0], TRUE);
+        throw new Resque_Job_DontPerform;
+      }
+      else {
+        throw new Resque_Job_DontPerform;
+      }
     }
   }
 
