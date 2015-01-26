f3dcd04262a082952f12c32d7402af36a7f1db5b
Zack Urben <zackurben@gmail.com>
2015-01-26 12:26:38 -0600

    Refactoring to implement QueuePlugin and for drupal cs, moved lock and unlock operations to functions to be used by other plugins.

--- lib/Drupal/resque/LockPlugin.php
+++ lib/Drupal/resque/LockPlugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @file
- * Contains Drupal\resque\PerformLockPlugin.
+ * Contains Drupal\resque\LockPlugin.
  *
  * Provides a resource lock for the Resque Job Perform function.
  */
@@ -13,14 +13,11 @@ use \Resque_Job;
 use \Resque_Job_DontPerform;
 
 /**
- * Provides semaphore-like behavior for Reque Jobs, by using the event hooks.
+ * Provides semaphore-like behavior for Resque Jobs, by using the event hooks.
  */
 class LockPlugin {
   /**
-   * Check if the job can acquire the processing lock.
-   *
-   * This will requeue the given job upon failure, if the requeue parameter is
-   * set to true.
+   * Attempts to acquire the jobs unique lock, or requeues the job.
    *
    * @param Resque_Job $job
    *   The resque job.
@@ -28,20 +25,18 @@ class LockPlugin {
    * @return bool
    *   If the job was acquired.
    *
-   * @throws Resque_Job_DontPerform
+   * @throws \Resque_Job_DontPerform
    */
   public static function beforePerform(Resque_Job $job) {
-    if (Php_Resque::redis()->setnx($job->payload['args'][0]['drupal_unique_key'], '1')) {
-      return TRUE;
+    // Attempt to acquire the job lock.
+    if (!self::lockJob($job)) {
+      // Attempt to requeue this job.
+      QueuePlugin::requeueJob($job);
+      throw new \Resque_Job_DontPerform();
     }
     else {
-      if ($job->payload['args'][0]['requeue']) {
-        Php_Resque::enqueue($job->queue, $job->payload['class'], $job->payload['args'][0], TRUE);
-        throw new Resque_Job_DontPerform();
-      }
-      else {
-        throw new Resque_Job_DontPerform();
-      }
+      // The job was acquired.
+      return TRUE;
     }
   }
 
@@ -53,9 +48,8 @@ class LockPlugin {
    *   The job that failed.
    */
   public static function afterPerform(Resque_Job $job) {
-    if (Php_Resque::redis()->exists($job->payload['args'][0]['drupal_unique_key'])) {
-      Php_Resque::redis()->del($job->payload['args'][0]['drupal_unique_key']);
-    }
+    // Clear the job lock.
+    self::unlockJob($job);
   }
 
   /**
@@ -65,10 +59,56 @@ class LockPlugin {
    *   Exception that occurred.
    * @param Resque_Job $job
    *   The job that failed.
+   *
+   * @throws \Exception
    */
   public static function onFailure($exception, Resque_Job $job) {
-    if (Php_Resque::redis()->exists($job->payload['args'][0]['drupal_unique_key'])) {
-      Php_Resque::redis()->del($job->payload['args'][0]['drupal_unique_key']);
+    // Clear the job lock.
+    self::unlockJob($job);
+  }
+
+  /**
+   * Lock the given job, using the drupal_unique_key defined in the payload.
+   *
+   * @param Resque_Job $job
+   *   The base job to lock.
+   *
+   * @return bool
+   *   If the job was locked.
+   */
+  public static function lockJob(Resque_Job $job) {
+    if (Php_Resque::redis()->setnx(
+      $job->payload['args'][0]['drupal_unique_key'],
+      '1'
+    )) {
+      return TRUE;
+    }
+    else {
+      return FALSE;
+    }
+  }
+
+  /**
+   * Unlock the given job, using the drupal_unique_key defined in the payload.
+   *
+   * @param Resque_Job $job
+   *   The base job to unlock.
+   *
+   * @return bool
+   *   If the job was unlocked.
+   */
+  public static function unlockJob(Resque_Job $job) {
+    if (Php_Resque::redis()->exists(
+      $job->payload['args'][0]['drupal_unique_key']
+    )) {
+      Php_Resque::redis()->del(
+        $job->payload['args'][0]['drupal_unique_key']
+      );
+
+      return TRUE;
+    }
+    else {
+      return FALSE;
     }
   }
 }
